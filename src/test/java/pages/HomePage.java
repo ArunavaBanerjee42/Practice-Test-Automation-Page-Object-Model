ca9b9a6ba3bce4b89d55b00acda7456f5fde481f
thunderop9069-create <thunderop9069@gmail.com>
2026-01-01 10:37:23 +0530

    apply page factory methods to the existing HomePage.java and LoginPage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,34 +5,45 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
-	private By form = By.xpath("//*[@id=\"mp_form_below_posts7\"]/form");
-	private By formNameInput = By.xpath("//*[@id=\"form_first_name_7\"]");
-	private By formEmailInput = By.xpath("//*[@id=\"form_email_7\"]");
-	private By xpathCheatSheetButton = By.xpath("//*[@id=\"mp_form_below_posts7\"]/form/div[3]/input");
+	@FindBy(xpath = "//*[@id='mp_form_below_posts7']/form")
+	private WebElement form;
+
+	@FindBy(xpath = "//*[@id='form_first_name_7']")
+	private WebElement formNameInput;
+
+	@FindBy(xpath = "//*[@id='form_email_7']")
+	private WebElement formEmailInput;
+
+	@FindBy(xpath = "//*[@id='mp_form_below_posts7']/form/div[3]/input")
+	private WebElement xpathCheatSheetButton;
+
 	WebDriver driver;
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	public void scrollToTheForm() {
 	    JavascriptExecutor js = (JavascriptExecutor) driver;
 	    js.executeScript(
 	        "arguments[0].scrollIntoView(true)",
-	        driver.findElement(form)
+	        form
 	    );
 	}
-	
 	public void enterName(String name) {
-		driver.findElement(formNameInput).sendKeys(name);
+		formNameInput.sendKeys(name);
 	}
 	public void enterEmail(String email) {
-		driver.findElement(formEmailInput).sendKeys(email);
+		formEmailInput.sendKeys(email);
 	}
 	public void clickButton() {
-		driver.findElement(xpathCheatSheetButton).click();
+		xpathCheatSheetButton.click();
 	}
 	public String checkTitle() {
 	    By heading = By.xpath("//*[@id='loop-container']//h1");
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,73 +1,46 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
-    private By userNameInput;
-    private By passwordInput;
-    private By submitButton;
-    private By errorMessage;
+	
+	@FindBy (id = "username")
+    private WebElement userNameInput;
+	
+	@FindBy (id = "password")
+    private WebElement passwordInput;
+	
+	@FindBy (id = "submit")
+    private WebElement submitButton;
+	
+	@FindBy (id = "error")
+    private WebElement errorMessage;
 
     WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         driver.get("https://practicetestautomation.com/practice-test-login/");
-
-        setUserNameInput(By.id("username"));
-        setPasswordInput(By.id("password"));
-        setSubmitButton(By.id("submit"));
-        setErrorMessage(By.id("error"));
+        PageFactory.initElements(driver, this);
+        
     }
-
-    // Getters & Setters
-    public By getUserNameInput() {
-        return userNameInput;
-    }
-
-    public void setUserNameInput(By userNameInput) {
-        this.userNameInput = userNameInput;
-    }
-
-    public By getPasswordInput() {
-        return passwordInput;
-    }
-
-    public void setPasswordInput(By passwordInput) {
-        this.passwordInput = passwordInput;
-    }
-
-    public By getSubmitButton() {
-        return submitButton;
-    }
-
-    public void setSubmitButton(By submitButton) {
-        this.submitButton = submitButton;
-    }
-    
-	public By getErrorMessage() {
-	    return errorMessage;
-	}
-	public void setErrorMessage(By errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-
 
     // Actions
     public void enterUsername(String username) {
-        driver.findElement(userNameInput).clear();
-        driver.findElement(userNameInput).sendKeys(username);
+        userNameInput.clear();
+        userNameInput.sendKeys(username);
     }
 
     public void enterPassword(String password) {
-        driver.findElement(passwordInput).clear();
-        driver.findElement(passwordInput).sendKeys(password);
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
     }
 
     public void clickSubmit() {
-        driver.findElement(submitButton).click();
+        submitButton.click();
     }
     
 
@@ -89,7 +62,7 @@ public class LoginPage {
     }
     
     public boolean isErrorMessageDisplayed() {
-    	return driver.findElement(errorMessage).isDisplayed();
+    	return errorMessage.isDisplayed();
     }
 
 	
